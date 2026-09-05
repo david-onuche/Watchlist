1d229019db0b95a7c685675e0d72c7ef0b326ecb
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 00:26:39 +0100

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by David Onuche on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
