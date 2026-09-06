d995869b93357249dc60ea336fca7a94cd256e5b
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 10:37:12 +0100

    Fetching Data from Database

--- Watchlist/Component/ButtonImageView.swift
+++ Watchlist/Component/ButtonImageView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ButtonImageView: View {
+    let sysmbolName: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: sysmbolName)
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(.blue.gradient)
+            .padding(8)
+            .background(
+                Circle()
+                    .fill(.ultraThickMaterial)
+            )
+            .frame(width: 80)
     }
 }
 
 #Preview {
-    ButtonImageView()
+    ButtonImageView(sysmbolName: "plus.circle.fill")
 }
